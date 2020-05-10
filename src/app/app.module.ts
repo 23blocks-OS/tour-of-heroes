d0ae29054cce45d836d52c8e4663cd6e1e1a8566
Michael <maikol.merchan23@gmail.com>
2020-05-10 12:54:41 -0500

    Heroes fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,50 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+
 import { AppComponent }         from './app.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { MessagesComponent }    from './messages/messages.component';
+
 import { AppRoutingModule }     from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
 import {ReactiveFormsModule} from '@angular/forms';
 import {environment} from '../environments/environment';
 import {HttpClientModule} from '@angular/common/http';
+import { metaReducers, reducers } from './core/reducers';
+import {EffectsModule} from '@ngrx/effects';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {Gateway23blocksModule} from './core/23blocks/gateway/gateway-23blocks.module';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    AppComponent,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, {metaReducers}),
+    EffectsModule.forRoot([]),
     Gateway23blocksModule.forRoot({
       apiBase: environment.API_23GATEWAY_URL,
       APPID: environment.APPID,
       registerAccountCallback: environment.APP_URL + '/auth/step2/',
-      resetPasswordCallback: environment.APP_URL + '/auth/change-password/'}),
-
-  ],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
+      resetPasswordCallback: environment.APP_URL + '/auth/change-password/'
+    }),
     ReactiveFormsModule,
     BrowserAnimationsModule
   ],
   providers: [
     Gateway23blocksModule],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
